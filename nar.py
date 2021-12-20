ae2c1e5bd0854169185d82356476f491f90884ea
pri2darshini <pri2darshini@gmail.com>
2021-12-20 10:28:17 +0000

    Create nar.py

---
+++ nar.py
@@ -0,0 +1,21 @@
+def narcissistic( a ):
+    
+    n = a
+    add_digit=0
+    b = len(str(a))
+    
+
+    for i in range(b):
+        print(add_digit)
+        print(n)
+        temp = n%10
+        add_digit += temp**b
+        n=int(n/10)
+        
+    if a == add_digit:
+        return True
+    else:
+        return False
+
+
+print(narcissistic(371))
